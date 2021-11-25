4f407f348c61e98d3b62fd8ae71b3cea74005585
lakshmanswamiyalla <94294050+lakshmanswamiyalla@users.noreply.github.com>
2021-11-25 06:57:38 +0530

    Add files via upload

---
+++ 3_Implementation/INC/header.h
@@ -0,0 +1,13 @@
+
+#ifndef _head_H
+#define _head_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void convert_to_x_base(int a, int b);
+#define note "maze mat lo, enter the valid operation"
+void binary();
+void decimal();
+void octal();
+void hexdecimal();
+#endif
